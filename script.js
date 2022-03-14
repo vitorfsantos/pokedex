c3a76beb34cf995d9b79addd24ee03a428ee4576
vitorfsantos <91084782+vitorfsantos@users.noreply.github.com>
2022-03-14 10:27:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const listaSelecaoPokemon = document.querySelectorAll('.pokemon')
+const pokemonCard = document.querySelectorAll('cartao-pokemon')
+
+
+
+listaSelecaoPokemon.forEach(pokemon =>{
+    pokemon.addEventListener('click',()=>{
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir =  document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+    })
+})
